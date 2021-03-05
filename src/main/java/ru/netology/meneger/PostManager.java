8e63739aba89de5f85876f102aae0a7f65814012
DarcenRal <75911924+DarcenRal@users.noreply.github.com>
2021-03-05 23:10:21 +0300

    Added Post Manager V2.0

---
+++ src/main/java/ru/netology/meneger/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.meneger;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        // TODO: add logic
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+}

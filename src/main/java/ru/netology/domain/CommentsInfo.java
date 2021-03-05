faa3563eb869d9507e0e3531adc01fb96be5ae5f
DarcenRal <75911924+DarcenRal@users.noreply.github.com>
2021-03-05 22:34:52 +0300

    Added Post, CommentsInfo, Change Pom

---
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -0,0 +1,50 @@
+package ru.netology.domain;
+
+public class CommentsInfo {
+    private String logoCommenter;
+
+    public String getLogoCommenter() {
+        return logoCommenter;
+    }
+
+    public void setLogoCommenter(String logoCommenter) {
+        this.logoCommenter = logoCommenter;
+    }
+
+    public String getCommenterId() {
+        return commenterId;
+    }
+
+    public void setCommenterId(String commenterId) {
+        this.commenterId = commenterId;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public String getDateComment() {
+        return dateComment;
+    }
+
+    public void setDateComment(String dateComment) {
+        this.dateComment = dateComment;
+    }
+
+    public String getTimeComment() {
+        return timeComment;
+    }
+
+    public void setTimeComment(String timeComment) {
+        this.timeComment = timeComment;
+    }
+
+    private String commenterId;
+    private String commentText;
+    private String dateComment;
+    private String timeComment;
+}
